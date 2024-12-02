d7c97503eb7fba6b911fb0d9e1a352c0d150753b
razzaksr <razzaksr@gmail.com>
2024-12-02 12:21:15 +0530

    Load balancing and auto scaling enabled

--- customer-service/src/main/java/microservices/one/customer_service/FeignConnector.java
+++ customer-service/src/main/java/microservices/one/customer_service/FeignConnector.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8072",name = "Feign-Connector")
+//@FeignClient(url = "http://localhost:8072",name = "Feign-Connector")
+@FeignClient(name = "ACCOUNTS-SERVICE")
 public interface FeignConnector {
     @GetMapping("/holder/{customer}")
     List<Account> receiveAccountsFromService(@PathVariable("customer") int customer);
